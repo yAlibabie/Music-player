4bb8e336189f3d14b61c278a9cb051d49ab586ac
Y.Alibabaie <yasamin.alibabaie.1081@gmail.com>
2025-04-18 13:43:48 +0330

    PremiumBehavior class was written

--- music-player/src/fp/PremiumBehavior.java
+++ music-player/src/fp/PremiumBehavior.java
@@ -1,4 +1,30 @@
 package fp;
 
-public class PremiumBehavior {
+import static fp.Music.allMusics;
+
+public class PremiumBehavior implements UserBehavior {
+    private int month;
+
+    public  PremiumBehavior (int month) {
+        this.month = month;
+    }
+
+    @Override
+    public void createPlaylist(String Title, User Owner) {
+        Playlist newPlaylist = new Playlist(Title, Owner);
+    }
+
+    @Override
+    public void playMusic(Music music) throws InvalidOperationException {
+        if(!allMusics.contains(music)) {
+            throw new InvalidOperationException("this music does not exist.");
+        }
+
+        music.play();
+    }
+
+    @Override
+    public void buyPremium(User owner, int month) {
+        this.month += month;
+    }
 }
