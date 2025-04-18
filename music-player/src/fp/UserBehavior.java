4b344839fe03ee148c33e2d989dada3258509ef4
Y.Alibabaie <yasamin.alibabaie.1081@gmail.com>
2025-04-18 13:42:13 +0330

    UserBehavior interface was writen for regular and premium behaviors

--- music-player/src/fp/UserBehavior.java
+++ music-player/src/fp/UserBehavior.java
@@ -1,4 +1,7 @@
 package fp;
 
 public interface UserBehavior {
+    void createPlaylist (String Title, User Owner);
+    void playMusic (Music music);
+    void buyPremium (User owner, int month);
 }
